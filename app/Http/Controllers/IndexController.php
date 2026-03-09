e5733f438c3965842f3e0a7b4bd6117bc6c7496e
thomas <thomas.zeryouh@goons.fr>
2026-03-09 14:17:56 +0100

    act 3

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return 'Bienvenue sur la page d\'accueil';
+    }
+}
