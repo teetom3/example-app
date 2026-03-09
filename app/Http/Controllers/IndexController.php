ebc4a71f0a21f493dcafeb308076eca882fb0447
thomas <thomas.zeryouh@goons.fr>
2026-03-09 15:11:04 +0100

    act4-5

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,6 +6,8 @@ class IndexController extends Controller
 {
     public function index()
     {
-        return 'Bienvenue sur la page d\'accueil';
+        $name = 'Thomas';
+
+        return view('welcome', compact('message'));
     }
 }
