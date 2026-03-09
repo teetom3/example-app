d6d68248278d670045c2eeb9763b92fbc13d7d2c
thomas <thomas.zeryouh@goons.fr>
2026-03-09 16:17:20 +0100

    act8

---
+++ resources/views/errors/not-found.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page non trouvée</title>
+</head>
+<body>
+    <h1>Page non trouvée</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use App\Http\Controllers\IndexController;
 
 Route::get('/', [IndexController::class, 'index'])->name('home');
 Route::get('/articles/{id}', [IndexController::class, 'show'])->name('article.details');
+
+Route::fallback(function () {
+    return view('errors.not-found');
+});
