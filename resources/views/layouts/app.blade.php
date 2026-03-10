7f0c2ca36f77c32dde496fcdf81123b4d40cd132
thomas <thomas.zeryouh@goons.fr>
2026-03-10 14:04:20 +0100

    act 15

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
 </head>
 <body>
     @include('partials.header')
